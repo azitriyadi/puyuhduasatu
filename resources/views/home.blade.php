945e7b5ad6356601cf5c893cd349c1c8aa024d0c
azitriyadi <azitriyadi@gmail.com>
2025-06-14 01:09:58 +0700

    tambah contak

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -184,6 +184,23 @@
     </div>
   </section>
   <!--akhir review-->
+  <!--contanct-->
+  <section class = "contact" id="contact">
+    <h1 class="heading"><span> contact </span> us </h1>
+    <div class="row">
+      <form action="">
+        <input type="text" placeholder="home" class="box">
+        <input type="email" placeholder="email" class="box">
+        <input type="number" placeholder="number" class="box">
+        <textarea name="" class="box" placeholder="message" id="" clos="30" rows="10"></textarea>
+        <input type="submit" value="send messge" class="btn">
+      </form>
+      <div class="image">
+        <img src="#" alt="gambar telur">
+      </div>
+    </div>
+
+  </section>
 
 </body>
 
